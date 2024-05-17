519a62b32237a1f9305e56c04e4beab208723a49
DenSad81 <tagil81@mail.ru>
2024-05-17 16:16:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int minDigit = 2;
+        int maxDigit = 999;
+        int argument = 2;
+        int sum;
+        int exponent;
+        Random random = new Random();
+        int digit = random.Next(minDigit, (maxDigit + 1));
+
+        sum = argument;
+        for (exponent = 1; sum <= digit; exponent++)
+            sum *= argument;
+
+        Console.WriteLine($"Число {digit }, число {argument} в степени {exponent} = {sum}");
+    }
+}
